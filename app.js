4a801f581e712af856b16e84008139895ae28f2f
Jordan Larios <jolarios96@gmail.com>
2022-05-31 21:01:24 -0500

    finished main game logic; Found a problem with unsolvable solutions

--- app.js
+++ app.js
@@ -35,17 +35,81 @@ for (let n = 0; n < 25; n++) {
 
 // console.log(document.querySelectorAll("#app>.container>.light"));
 // select all .light class elements within #app > .container
-document.querySelectorAll("#app>.container>.light").forEach((element) => {
+document.querySelectorAll(".light").forEach((element) => {
+  let value = parseInt(element.value);
+
   // Toggle light state
   element.addEventListener("click", function () {
-    if (light_state_arr[element.value]) {
-      element.style.backgroundColor = "var(--btn-off)";
-      light_state_arr[element.value] = false;
-      console.log(light_state_arr);
-    } else {
-      element.style.backgroundColor = "var(--btn-on)";
-      light_state_arr[element.value] = true;
-      console.log(light_state_arr);
+    // toggle middle light
+    toggleLight(value);
+
+    // toggle north light
+    if (value - 5 > -1) toggleLight(value - 5);
+
+    // toggle east light
+    switch (value + 1) {
+      case 0:
+        break;
+      case 5:
+        break;
+      case 10:
+        break;
+      case 15:
+        break;
+      case 20:
+        break;
+      default:
+        if (value + 1 < 25) toggleLight(value + 1);
+        break;
+    }
+
+    // toggle south light
+    if (value + 5 < 25) toggleLight(value + 5);
+
+    // toggle west light
+    switch (value - 1) {
+      case 4:
+        break;
+      case 9:
+        break;
+      case 14:
+        break;
+      case 19:
+        break;
+      case 24:
+        break;
+      default:
+        if (value - 1 >= 0) toggleLight(value - 1);
+        break;
     }
   });
 });
+
+function toggleCenter(element) {
+  let value = parseInt(element.value);
+
+  if (light_state_arr[value]) {
+    element.style.backgroundColor = "var(--btn-off)";
+    light_state_arr[value] = false;
+    console.log(light_state_arr);
+  } else {
+    element.style.backgroundColor = "var(--btn-on)";
+    light_state_arr[value] = true;
+    console.log(light_state_arr);
+  }
+}
+
+function toggleLight(value) {
+  console.log(value);
+  let element = document.querySelector("[value='" + value + "']");
+
+  if (light_state_arr[value]) {
+    element.style.backgroundColor = "var(--btn-off)";
+    light_state_arr[value] = false;
+    console.log(light_state_arr);
+  } else {
+    element.style.backgroundColor = "var(--btn-on)";
+    light_state_arr[value] = true;
+    console.log(light_state_arr);
+  }
+}
